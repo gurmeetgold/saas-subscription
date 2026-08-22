eebc10f6341b91f2ae21c3546b6584928f989791
gurmeetgold <50425243+gurmeetgold@users.noreply.github.com>
2026-08-22 16:29:49 -0400

    Add Spring Boot application entry point

---
+++ docker-k8s-lab/src/main/java/com/gurmeet/dockerk8slab/DockerK8sLabApplication.java
@@ -0,0 +1,11 @@
+package com.gurmeet.dockerk8slab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerK8sLabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerK8sLabApplication.class, args);
+    }
+}
